b3f5ac50f5d293753f9498ae8e1909067ccb6927
takabooo <61100168+takabooo@users.noreply.github.com>
2020-02-16 11:43:56 +0900

    add

---
+++ shooting/Assets/corona_sc.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class corona_sc : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.transform.position += new Vector3(0, -9, 0) * Time.deltaTime;
+
+        if (this.transform.position.y < -5)
+            Destroy(this.gameObject);
+
+	}
+}
